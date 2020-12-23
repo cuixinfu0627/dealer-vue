dcbfb8c866d4376e10806a37f8197dc75fb460df
cuixinfu <870459550@qq.com>
2020-12-23 21:44:56 +0800

    修改域名为https域名

--- static/config/index-uat.js
+++ static/config/index-uat.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://api.longma.cykonhy.com';
+  window.SITE_CONFIG['baseUrl'] = 'https://api.longma.cykonhy.com';
 
   // img接口请求地址
   window.SITE_CONFIG['baseImg'] = 'http://q854zv8fy.bkt.clouddn.com/';
