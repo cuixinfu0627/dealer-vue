4163aa4ec2c024e2a14644f83e2dc55080abe7ed
cuixinfu <870459550@qq.com>
2021-10-09 09:49:05 +0800

    新增升级嘉德视频配送分支-1.2版本

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 // 公共变量
-const uploadHeader = "https://api.dealer.cykonhy.com/sys/user/uploadAavatar";
+const uploadHeader = "https://api.jiade.cykonhy.com/sys/user/uploadAavatar";
 
 // 默认图片地址:
 const headImgUrl = "http://wx.qlogo.cn/mmhead/NFTP6g88OibUZHkPI0KpQvtsnjYz3qQUYMlPAibM3LFqc/132";
